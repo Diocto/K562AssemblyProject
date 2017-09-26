358b0e31021b4c67b54de4267409c6b7b4c194ca
kwonsg <kwonsg1116@gmail.com>
2017-09-26 22:55:57 +0900

    change extract_bp.cc

--- extract_bp.cc
+++ extract_bp.cc
@@ -5,10 +5,14 @@
 #include<vector>
 #include<algorithm>
 #include<set>
+#include<sstream>
 
 using namespace std;
+
+stringstream ss;
+
 void read_garbage(int num,ifstream& is);
-void print_error(string Error, string ref_chr_num[], string input_chr_num[], unsigned long long ref_chr_start[],unsigned long long ref_chr_end[], string contig[] );
+void print_error(string Error, string ref_chr_num[], string input_chr_num[], unsigned long long ref_chr_start[],unsigned long long ref_chr_end[], string contig[], long long ref_gap,  long long input_gap );
 
 int main(int argc, char* argv[])
 {
@@ -27,17 +31,22 @@ int main(int argc, char* argv[])
   contig[0] = "";
 
   read_garbage(15, is);
-  unsigned long long ref_gap = 0;
-  unsigned long long input_gap = 0;
+  long long ref_gap = 0;
+  long long input_gap = 0;
   int i = 0;
+  double Dup_cnt = 0;
+  double Inv_cnt = 0;
+  double Ins_cnt = 0;
+  double Del_cnt = 0;
+  double Total_cnt ;
  
   cout<<"#FORMAT = text"<<endl;
   cout<<"#REFERENCE = "<<ref<<endl;
   cout<<"#CANCER = "<<cancer1<<", "<<cancer2<<endl;
-  cout<<"#ERROR_TYPE = Translocation"<<endl; 
+  cout<<"#ERROR_TYPE = Duplication"<<endl; 
   cout<<"#ERROR_TYPE = Inversion"<<endl; 
   cout<<"#ERROR_TYPE = Deletion"<<endl; 
-  cout<<"#ERROR_TYPE = Insertion"<<endl<<endl<<endl; 
+  cout<<"#ERROR_TYPE = Insertion"<<endl<<endl; 
 
   while(!is.eof())
   { 
@@ -52,13 +61,25 @@ int main(int argc, char* argv[])
       input_gap = input_chr_start[1] - input_chr_end[0];
       
       if(ref_gap > 0 && input_gap > 0){
-        if( ref_gap == input_gap && !complement[0].compare(complement[1]) && !complement[0].compare("-"))
-          print_error("Translocation",ref_chr_num, input_chr_num,ref_chr_start,ref_chr_end,contig);
-        else if( ref_gap < input_gap)
-          print_error("Insertion",ref_chr_num, input_chr_num,ref_chr_start,ref_chr_end,contig);
-        else if(ref_gap > input_gap)
-          print_error("Deletion",ref_chr_num, input_chr_num,ref_chr_start,ref_chr_end,contig);
+        if( ref_gap == input_gap && !complement[0].compare(complement[1]) && !complement[0].compare("-")){
+          print_error("Inversion",ref_chr_num, input_chr_num,ref_chr_start,ref_chr_end,contig,ref_gap,input_gap);
+    	  Inv_cnt++;
+        }
+        else if( ref_gap < input_gap){
+          print_error("Insertion",ref_chr_num, input_chr_num,ref_chr_start,ref_chr_end,contig,ref_gap,input_gap);
+   	  Ins_cnt++;
+        }
+        else if(ref_gap > input_gap){
+          print_error("Deletion",ref_chr_num, input_chr_num,ref_chr_start,ref_chr_end,contig,ref_gap,input_gap);
+	  Del_cnt++;
+	}
       }
+     else{
+        if(!complement[0].compare(complement[1]) && !contig[0].compare(contig[1])){
+          print_error("Duplication",ref_chr_num, input_chr_num,ref_chr_start,ref_chr_end,contig,ref_gap,input_gap);
+          Dup_cnt++; 
+        }
+     }
     }
    
     ref_chr_num[0] = ref_chr_num[1];
@@ -73,6 +94,14 @@ int main(int argc, char* argv[])
    
     read_garbage(2,is); 
   }
+  Total_cnt = Dup_cnt + Ins_cnt + Inv_cnt + Dup_cnt;
+  cout<<"#Total = "<<Total_cnt<<endl;
+  cout<<"#Duplication % = "<<Dup_cnt/Total_cnt*100<<"("<<Dup_cnt<<")"<<endl;
+  cout<<"#Inversion % = "<<Inv_cnt/Total_cnt*100<<"("<<Inv_cnt<<")"<<endl;
+  cout<<"#Insetion % =  "<<Ins_cnt/Total_cnt*100<<"("<<Ins_cnt<<")"<<endl;
+  cout<<"#Deletion % =  "<<Del_cnt/Total_cnt*100<<"("<<Del_cnt<<")"<<endl<<endl<<endl;
+
+  cout<<ss.str();
   return 0;
 }
 
@@ -85,8 +114,11 @@ void read_garbage(int num,ifstream& is)
   }
 }
 
-void print_error(string Error, string ref_chr_num[], string input_chr_num[], unsigned long long ref_chr_start[],unsigned long long ref_chr_end[], string contig[] ){
-  cout<<Error<<" : " << ref_chr_num[0]<<" ";
-  cout<<ref_chr_start[0]<<" "<<ref_chr_end[0]<<" "<<ref_chr_start[1]<<" "<<ref_chr_end[1]<<" contig"<<input_chr_num[0]<<endl;
-  cout<< "1. " <<contig[0]<<endl<<"2. "<<contig[1]<<endl<<endl;
+void print_error(string Error, string ref_chr_num[], string input_chr_num[], unsigned long long ref_chr_start[],unsigned long long ref_chr_end[], string contig[], long long ref_gap, long long input_gap ){
+  ss<<"#"<<Error << endl;
+  ss<<"#Ref Chromosome = "<< ref_chr_num[0]<<endl;
+  ss<<"#Input Contig = "<<input_chr_num[0]<<endl;
+  ss<<"#1's Position : Start = "<<ref_chr_start[0]<<", End = "<<ref_chr_end[0]<<", Size = "<<contig[0].size()<<endl;
+  ss<<"#2's Position : Start = "<<ref_chr_start[1]<<", End = "<<ref_chr_end[1]<<", Size = "<<contig[1].size()<<endl;
+  ss<< "1. " <<contig[0]<<endl<<"2. "<<contig[1]<<endl<<endl;
 }
